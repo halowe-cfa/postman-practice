4b643047dcdf9019dbd28e874d93f059c790169c
halowe-cfa <91778081+halowe-cfa@users.noreply.github.com>
2022-04-12 16:18:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -16,3 +16,15 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
 })
+
+//create a post request for a new route called user-info in your app.js
+//go to postman and choose a post request
+//create a key called username
+//add a random username for your pretend user
+//create a key called name
+//add a name for your pretend user
+//create a key called location
+//add a location for your pretend user
+//send your post request
+//take a screenshot and upload it with the link to your repo
+
